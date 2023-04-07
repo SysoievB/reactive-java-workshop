3e990c5fd0708ab6080ef086e809ba1f500d2f20
SysoievB <sysoiev.bogdan.ua@gmail.com>
2023-04-07 09:31:20 +0200

    added third task

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,10 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
 
 public class Exercise3 {
 
@@ -10,8 +14,13 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
-
+        List<Integer> listFromFlux = intNumbersFlux()
+                .collectList()//collecting to Mono<List<Integer>>
+                .block();//returns T value from Mono<T>
+        var listSize = Objects.requireNonNull(listFromFlux).size() > 0 ? listFromFlux.size() : -1;
+        System.out.println("List size: " + listSize);
+        System.out.println("List values: ");
+        listFromFlux.forEach(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
