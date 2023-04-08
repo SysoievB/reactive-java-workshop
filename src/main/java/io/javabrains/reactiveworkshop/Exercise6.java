9f12de87e4e651ad87a056d4f79b44a60cd8b753
SysoievB <sysoiev.bogdan.ua@gmail.com>
2023-04-08 12:33:02 +0200

    added 7 task

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -1,6 +1,11 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Optional;
+
+import static io.javabrains.reactiveworkshop.ReactiveSources.unresponsiveFlux;
+import static io.javabrains.reactiveworkshop.ReactiveSources.unresponsiveMono;
 
 public class Exercise6 {
 
@@ -10,11 +15,20 @@ public class Exercise6 {
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        // TODO: Write code here
+        /*Optional<String> value = unresponsiveMono().blockOptional(Duration.ofSeconds(5));
+        System.out.println("Value from mono: " + value);*/
+
+        //chatGPT
+        unresponsiveMono()
+                .timeout(Duration.ofSeconds(5))
+                .subscribe(
+                        val -> System.out.println("Value from mono: " + val),
+                        error -> System.err.println("Error occurred: " + error)
+                );
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        //unresponsiveFlux();
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -2,33 +2,59 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
-public class Exercise7 {
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFluxWithRepeat;
 
+public class Exercise7 {
 
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Print all values from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+        /*intNumbersFlux()
+                .log()
+                .filter(v -> v > 5)
+                .subscribe(System.out::println);*/
 
         // Print 10 times each value from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+        /*intNumbersFlux()
+                .log()
+                .filter(v -> v > 5)
+                .subscribe(val -> {
+                    for (int i = 1; i <= 10; i++) {
+                        System.out.println("Value: " + val + " - " + i + " time(s) was printed");
+                    }
+                });*/
 
         // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        // TODO: Write code here
+        /*intNumbersFlux()
+                .filter(v -> v > 5)
+                .take(3)
+                .subscribe(val -> {
+                    for (int i = 1; i <= 10; i++) {
+                        System.out.println("Value: " + val + " - " + i + " time(s) was printed");
+                    }
+                });*/
 
         // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        // TODO: Write code here
+        /*intNumbersFlux()
+                .filter(v -> v > 20)
+                .defaultIfEmpty(-1)
+                .subscribe(System.out::println);*/
 
         // Switch ints from intNumbersFlux to the right user from userFlux
-        // TODO: Write code here
+
 
         // Print only distinct numbers from intNumbersFluxWithRepeat
-        // TODO: Write code here
+        /*intNumbersFluxWithRepeat()
+                .distinct()
+                .subscribe(System.out::println);*/
 
         // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
-        // TODO: Write code here
+        /*intNumbersFluxWithRepeat()
+                .distinctUntilChanged()
+                .subscribe(System.out::println);*/
 
         System.out.println("Press a key to end");
         System.in.read();
